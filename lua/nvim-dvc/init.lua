02850eb1e4b63223bd9172361f011eee2075a86c
Gennaro Tedesco <gennaro@billie.io>
2021-11-25 10:20:21 +0100

    added DVCMetrics

--- lua/nvim-dvc/init.lua
+++ lua/nvim-dvc/init.lua
@@ -12,12 +12,10 @@ local function dvc_stages()
    else
 	  local dvc_yaml_file = dvc.get_dvc_yaml(dvc.git_root())
 	  if dvc_yaml_file == nil then
-		 print("no dvc.yaml file found")
 		 return nil
 	  else
 		 local stages_lines = {}
 		 for s in vim.fn.system("dvc stage list --names-only"):gmatch("[^\r\n]+") do
-			-- table.insert(stages_lines, string.match(s, "%S+"))
 			table.insert(stages_lines, s)
 		 end
 		 vim.api.nvim_exec([[:e ]]..dvc_yaml_file, false)
@@ -37,8 +35,37 @@ local function dvc_stages()
    end
 end
 
+local function dvc_metrics()
+   if dvc.git_root() == "" then
+	  print("not a git repository")
+	  return nil
+   else
+	  local dvc_yaml_file = dvc.get_dvc_yaml(dvc.git_root())
+	  if dvc_yaml_file == nil then
+		 return nil
+	  else
+		 local metrics_lines = {}
+		 for s in vim.fn.system("dvc metrics show --json | jq '.\"\".data' | grep json | cut -d'\"' -f2"):gmatch("[^\r\n]+") do
+			table.insert(metrics_lines, s)
+		 end
+		 local qf_list = {}
+		 for _, v in pairs(metrics_lines) do
+			table.insert(qf_list, {filename = v, lnum = 1, col = 1})
+		 end
+
+		 if config.loc == 'quickfix' then
+			vim.fn.setqflist(qf_list, ' ')
+			vim.cmd('copen')
+		 else
+			vim.fn.setloclist(0, qf_list, ' ')
+			vim.cmd('lopen')
+		 end
+	  end
+   end
+end
 
 return {
    dvc_stages = dvc_stages,
+   dvc_metrics = dvc_metrics,
 }
 
--- plugin/nvim-dvc.vim
+++ plugin/nvim-dvc.vim
@@ -3,6 +3,7 @@ if exists("g:loaded_nvim_dvc")
 endif
 
 command! DVCStages execute 'lua require("nvim-dvc").dvc_stages()'
+command! DVCMetrics execute 'lua require("nvim-dvc").dvc_metrics()'
 
 let g:loaded_nvim_dvc = 1
 
